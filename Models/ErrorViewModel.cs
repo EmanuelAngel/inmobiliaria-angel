e80b0d48d802704cc7597eeb371284271aad7eef
EmanuelAngel <emanuelangelsbr@gmail.com>
2025-08-21 18:00:54 -0300

    `dotnet new mvc` template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inmobiliaria_angel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
