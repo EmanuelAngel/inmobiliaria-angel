f71f064ad823168e1c9bb861f7e24520120ec056
EmanuelAngel <emanuelangelsbr@gmail.com>
2025-09-19 00:13:45 -0300

    feat(db): setup mariadb

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,14 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using inmobiliaria_angel.Models;
+using MySql.Data.MySqlClient;
 
 namespace inmobiliaria_angel.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly string _conn = "Server=localhost;User=root;Password=;Database=inmobiliaria_angel_test;Port=3306";
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -15,7 +17,40 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        List<UsuarioTest> usuariosTest = [];
+
+        using (var conn = new MySqlConnection(_conn))
+        {
+            var query = $@"
+                SELECT
+                    id,
+                    nombre,
+                    fecha_registro
+                FROM
+                    usuarios;
+            ";
+
+            using (MySqlCommand command = new(query, conn))
+            {
+                conn.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    usuariosTest.Add(new UsuarioTest
+                    {
+                        Id = reader.GetInt32("id"),
+                        Nombre = reader.GetString("nombre"),
+                        FechaRegistro = reader.GetString("fecha_registro"),
+                    });
+                }
+
+                conn.Close();
+            }
+        }
+
+        return View(usuariosTest);
     }
 
     public IActionResult Privacy()
---
+++ Models/UsuarioTest.cs
@@ -0,0 +1,8 @@
+namespace inmobiliaria_angel.Models;
+
+public class UsuarioTest
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string FechaRegistro { get; set; }
+}
