6dd630b7e3eda66eece73822de941ca1bbc4cde1
EmanuelAngel <emanuelangelsbr@gmail.com>
2026-08-11 17:49:33 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inmobiliaria_lab2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
